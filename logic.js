a5bb1d834970d3762f3f94467e607a046dbe4a4d
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2023-01-20 15:34:56 +0100

    added the comming soon page and a bit of logic

--- logic.js
+++ logic.js
@@ -4,32 +4,49 @@ DESCRIPTION: A digital friends book.
 LAST CHANGE: 15.01.2023
 */
 
-var fab = document.getElementById('moreBtn');
-var fabImg = document.getElementById('more');
+// handles the floating action buttons
+try {
+    var fab = document.getElementById('moreBtn');
+    var fabImg = document.getElementById('more');
 
-fab.addEventListener('click', function() {
-    var fabShare = document.getElementById('shareBtn');
-    var fabAdd = document.getElementById('addBtn');
+    fab.addEventListener('click', function() {
+        var fabShare = document.getElementById('shareBtn');
+        var fabAdd = document.getElementById('addBtn');
 
-    var visibility = window.getComputedStyle(fabShare).getPropertyValue('visibility');
+        var visibility = window.getComputedStyle(fabShare).getPropertyValue('visibility');
 
-    if(visibility == 'collapse') {
-        fabShare.style.visibility = 'visible';
-        fabAdd.style.visibility = 'visible';
-        fabImg.src = 'img/down_icon.png';
-    } else if(visibility == 'visible') {
-        fabShare.style.visibility = 'collapse';
-        fabAdd.style.visibility = 'collapse';
-        fabImg.src = 'img/more_icon.png';
-    }
-});
+        if(visibility == 'collapse') {
+            fabShare.style.visibility = 'visible';
+            fabAdd.style.visibility = 'visible';
+            fabImg.src = 'img/down_icon.png';
+        } else if(visibility == 'visible') {
+            fabShare.style.visibility = 'collapse';
+            fabAdd.style.visibility = 'collapse';
+            fabImg.src = 'img/more_icon.png';
+        }
 
+        fabAdd.addEventListener('click', function() {
+            parent.location.href = 'commingSoon.html';
+        });
+
+        fabShare.addEventListener('click', function() {
+            parent.location.href = 'commingSoon.html';
+        });
+    });
+} catch (error) {
+    
+}
+
+
+// handles clicking on a friend
 var items = document.getElementsByClassName('item');
 
 for(var i = 0; i < items.length; i++) {
     items[i].addEventListener('click', function() {
-        var id = this.id;
+        var name = this.getElementsByClassName('name')[0].innerHTML;
         parent.location.href = 'profile.html';
+        var profileName = document.getElementById('name');
+        profileName.innerHTML = name;
     });
 }
 
